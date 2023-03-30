fa766c4e27b110e7b4d2af0432ef516fefc978f0
Minsu Lee <minsulee@c4r6s4.42seoul.kr>
2023-03-30 15:00:44 +0900

    minor bug fix

--- srcs/run.c
+++ srcs/run.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   run.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yim <yim@student.42seoul.kr>               +#+  +:+       +#+        */
+/*   By: minsulee <minsulee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 14:33:01 by minsulee          #+#    #+#             */
-/*   Updated: 2023/03/30 14:39:14by yim              ###   ########.fr       */
+/*   Updated: 2023/03/30 15:00:03 by minsulee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,42 +160,40 @@ void	projection_direction_set(t_projection *projection)
 
 void	projection_shoot(t_map *map, t_projection *projection)
 {
-		int hit;
+	int hit;
 
-		while (projection->hit == 0)
+	while (projection->hit == 0)
+	{
+		//jump to next map square, either in x-direction, or in y-direction
+		if (projection->side_dist_x < projection->side_dist_y)
 		{
-			//jump to next map square, either in x-direction, or in y-direction
-			if (projection->side_dist_x < projection->side_dist_y)
-			{
-				projection->side_dist_x += projection->delta_dist_x;
-				projection->map_x += projection->step_x;
-				projection->side = 0;
-			}
-			else
-			{
-				projection->side_dist_y += projection->delta_dist_y;
-				projection->map_y += projection->step_y;
-				projection->side = 1;
-			}
-			//Check if ray has hit a wall
-			if (map->map[projection->map_y][projection->map_x] == '1') // FIX IT TO map_y map_x. IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!
-				projection->hit = 1;
+			projection->side_dist_x += projection->delta_dist_x;
+			projection->map_x += projection->step_x;
+			projection->side = 0;
 		}
-		// double perp_wall_dist;
-		if (projection->side == 0)
-			projection->perp_wall_dist = (projection->side_dist_x - projection->delta_dist_x);
 		else
-			projection->perp_wall_dist = (projection->side_dist_y - projection->delta_dist_y);
+		{
+			projection->side_dist_y += projection->delta_dist_y;
+			projection->map_y += projection->step_y;
+			projection->side = 1;
+		}
+		//Check if ray has hit a wall
+		if (map->map[projection->map_y][projection->map_x] == '1') // FIX IT TO map_y map_x. IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!
+			projection->hit = 1;
+	}
+}
 
-
-//이거 원래 height 다음에 있는건데 겹치는 거 없는 것 같아서 일단 위로 데리고 왔음!
-		if (projection->side == 0)
-			projection->wall_x = projection->pos_y + projection->perp_wall_dist * projection->raydir_y;
-		else
-			projection->wall_x = projection->pos_x + projection->perp_wall_dist * projection->raydir_x;
-		// if (x == SCREEN_WIDTH / 2)
-		// 	printf("side : %d, previous Wall_x : %f, ", projection->side, projection->wall_x);
-		projection->wall_x -= floor(projection->wall_x);
+void	projection_point(t_projection *projection)
+{
+	if (projection->side == 0)
+		projection->perp_wall_dist = (projection->side_dist_x - projection->delta_dist_x);
+	else
+		projection->perp_wall_dist = (projection->side_dist_y - projection->delta_dist_y);
+	if (projection->side == 0)
+		projection->wall_x = projection->pos_y + projection->perp_wall_dist * projection->raydir_y;
+	else
+		projection->wall_x = projection->pos_x + projection->perp_wall_dist * projection->raydir_x;
+	projection->wall_x -= floor(projection->wall_x);
 }
 
 void	projection_height(t_projection *projection)
@@ -260,8 +258,10 @@ void	projection_texture(t_tex *tex, t_projection *projection)
 
 void	projection_draw(t_vars *ml_mlx, t_projection *projection, int x)
 {
-	int	tex_x;
-	int	tex_y;
+	int		tex_x;
+	int		tex_y;
+	double	step;
+	double	texPos;
 
 	tex_x = projection->tex_width - (int)(projection->wall_x * projection->tex_width);
 	// if (x == SCREEN_WIDTH / 2)
@@ -269,9 +269,6 @@ void	projection_draw(t_vars *ml_mlx, t_projection *projection, int x)
 	// 	printf("pos_x : %f, pos_y : %f, raydir_x : %f, raydir_y : %f, perp_wall_dist : %f, Wall_x : %f, TEX_wIDTH : %f, tex_x : %d\n", projection->pos_x, projection->pos_y, projection->raydir_x, projection->raydir_y, projection->perp_wall_dist, projection->wall_x, projection->tex_width, tex_x);
 	// 	printf("\n");
 	// }
-	int	color;
-	double step;
-	double texPos;
 
 
 	if (projection->side == 3 || projection->side == 0)
@@ -282,61 +279,31 @@ void	projection_draw(t_vars *ml_mlx, t_projection *projection, int x)
 	{
 		tex_y = (int)(texPos) & (int)(projection->tex_height - 1);
 		texPos += step;
-		color = projection->texture[(int) projection->tex_width * tex_y + tex_x];
 		// ml_mlx_put_pixel(&ml_mlx->data, x, y, projection->texture[(int)projection->tex_width * tex_y + tex_x]);
-		ml_mlx_put_pixel(&ml_mlx->data, x, y, color);
+		ml_mlx_put_pixel(&ml_mlx->data, x, y, projection->texture \
+		[(int) projection->tex_width * tex_y + tex_x]);
 	}
 }
 
 int	project_once(t_vars *ml_mlx, t_map *map, t_player *player)
 {
+	t_projection projection;
 
-	//1. x축 -1 ~ 1 보면서 거리 계산 : 1920(window_width) 만큼 scan
-	//2. 계산된 각각의 거리 저장하기
-	//3. 저장된 거리에 따라 y축이 얼마나 길어야 하는지 계산
-	//4. 저장된 y축 길이에 따라 mlx_win 에 x = 0 ~ window_width - 1까지 세로축 그리가
-	//5. 그려진 그대로 출력 후 종료
-
-	// printf("PROJECT ONCE\n");
-
-	// ml_mlx_clear_window(ml_mlx);
 	ml_mlx_ceiling(ml_mlx, ml_mlx->map->ceiling);
 	ml_mlx_floor(ml_mlx, ml_mlx->map->floor);
-
-	t_projection projection;
-	
-	char **worldMap = ml_mlx->map->map;
-
-	double pos_x = player->pos_x;
-	// pos_x = 22; // test
-	double pos_y = player->pos_y;
-	// pos_y = 12; // test
-
 	int x = 0, y = 0;
 	while (x < SCREEN_WIDTH)
 	{
 		projection_init(&projection, x, player);
 		projection_direction_set(&projection);
 		projection_shoot(ml_mlx->map, &projection);
+		projection_point(&projection);
 		projection_height(&projection);
 		projection_side(&projection);
 		projection_texture(&ml_mlx->tex, &projection);
 		projection_draw(ml_mlx, &projection, x);
 		x++;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
 	mlx_put_image_to_window(ml_mlx->mlx, ml_mlx->win, ml_mlx->data.img, 0, 0);
 	return (0);
 }
